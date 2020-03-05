e04945f8340c0916dda8a05788a63a3500808676
mynkit <dcm5124289@gmail.com>
2020-03-05 14:39:20 +0900

    再起動ロジックの変更

--- tor_test.py
+++ tor_test.py
@@ -8,20 +8,36 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 
 
-proxies = {
+PROXIES = {
     'http': 'socks5://127.0.0.1:9050',
     'https': 'socks5://127.0.0.1:9050'
 }
 
-if __name__ == '__main__':
-    subprocess.call(['service', 'tor', 'restart'])
 
-    LOGGER.info(f'proxies: {proxies}')
+def get_ipinfo(proxies=None) -> dict:
+    return requests.get('https://ipinfo.io', proxies=proxies).json()
+
+
+def tor_restart():
+    '''torを再起動してipアドレスを変更する
+    '''
+    subprocess.call(['killall', 'tor'])
+    subprocess.call(['service', 'tor', 'start'])
+
+
+if __name__ == '__main__':
+    tor_restart()
+    LOGGER.info(f'proxies: {PROXIES}')
 
     LOGGER.info('proxiesの設定なし')
-    res = requests.get('https://ipinfo.io').json()
+    res = get_ipinfo()
     LOGGER.info(f'res: {res}')
 
     LOGGER.info('proxiesの設定あり')
-    res = requests.get('https://ipinfo.io', proxies=proxies).json()
+    res = get_ipinfo(PROXIES)
+    LOGGER.info(f'res: {res}')
+
+    LOGGER.info('torの再起動')
+    tor_restart()
+    res = get_ipinfo(PROXIES)
     LOGGER.info(f'res: {res}')
